a6b02a3c2112f06c1576796ce04f51c306b36728
Doan Viet Thang <74363928+ThangDoan2001@users.noreply.github.com>
2023-04-18 13:43:29 +0700

    add: api get info

--- src/components/Connect/UserInfoDialog/index.js
+++ src/components/Connect/UserInfoDialog/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useContext}from 'react'
+import React, {useState, useContext, useEffect}from 'react'
 import {
   Button,
   Dialog,
@@ -10,10 +10,22 @@ import {
 import CloseIcon from "@mui/icons-material/Close";
 import InformationRow from '../../../shared/InformationRow';
 import { GlobalContext } from '../../../context/GlobalState';
+import { SERVER } from '../../../shared/constant/constant';
+import BigNumber from "bignumber.js"
+import { fetchData } from '../../../shared/utils/others';
 
 const UserInfoDialog = ({open, handleClose}) => {
   const [loading, setLoading] = useState(false);
+  const [userInfo, setUserInfo] = useState(null)
   const { balance, address } = useContext(GlobalContext)
+  
+  useEffect(() => {
+    fetchData({ public_key: address }, SERVER + '/wallet/info')
+      .then(data => setUserInfo(data))
+      
+  }, [address])
+
+  console.log(userInfo)
 
   const handleCloseDialog = () => {
     handleClose()
@@ -39,9 +51,9 @@ const UserInfoDialog = ({open, handleClose}) => {
       />
     <DialogContent>
     <InformationRow title="Address" value={address} bold={true}/>
-    <InformationRow title="Balance ETH" value={balance.eth + " ETH"} bold={true}/>
-    <InformationRow title="Transaction Amount" value="1000 ETH" bold={true}/>
-    <InformationRow title="Liquidatation number" value="0" bold={true}/>
+    <InformationRow title="Balance ETH" value={(userInfo ? BigNumber(userInfo.eth_balance).toFixed(2) : 0) + " ETH"} bold={true}/>
+    <InformationRow title="Transaction Amount" value={(userInfo ? BigNumber(userInfo.transaction_amount).toFixed(2) : 0) + " ETH"} bold={true}/>
+    <InformationRow title="Liquidatation number" value={userInfo ? userInfo.liquidation_number : 0}  bold={true}/>
     <Typography textAlign={"right"} sx={{fontSize: "10px", fontWeight: 600, marginTop: "20px", marginRight: "10px"}}>*These informations are collected by Seda Team</Typography>
     </DialogContent>
   </Dialog>  
--- src/shared/constant/constant.js
+++ src/shared/constant/constant.js
@@ -1,7 +1,9 @@
 const SEPOLIA_RPC = "https://rpc.sepolia.org"
 const MONGO_DB = "mongodb+srv://admin:Busitech123@busitech.brgm7jz.mongodb.net/test"
+const SERVER = "http://localhost:8000"
 
 module.exports = {
   SEPOLIA_RPC,
-  MONGO_DB
+  MONGO_DB,
+  SERVER
 }
